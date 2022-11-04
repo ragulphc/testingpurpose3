d07748d5f9cbbb87381f59b0df698c2f8cbf8360
ragulphc <ragul.p@avasoft.com>
2022-11-04 15:49:37 +0530

    Initial commit

---
+++ service-config.php
@@ -0,0 +1,12 @@
+<?php
+    $DEBUG = true;
+    
+    use afsdadsf_Env\EnvReader;
+    
+    (new EnvReader(afsdadsf_dir . '/env/config.env'))->load();
+    
+    $afsads = getenv(afsads); 
+$asdfasdf = getenv(asdfasdf); 
+
+
+?>
---
+++ servicelayer.php
@@ -0,0 +1,42 @@
+<?php 
+ 
+ /**
+    * afsdadsf 
+    *
+    * @package          afdsads
+    * @author           fasdfasdfasd 
+    * @copyright        fads   
+    * @license          fasfasdads 
+    * 
+    * @wordpress-plugin
+    * Plugin Name:      afsdadsf
+    * Plugin URI:       asdf 
+    * Description:      asdfasdf
+    * Version:          adsfasdfdasf 
+    * Author:           asfdasdfasdf
+    * Author URI:       asdfasdfasdf
+    * Text Domain:      asfdasdfasd 
+    * License:          fsdaasdfads 
+    * License URI:      fasdfsadfasd 
+    */ 
+ 
+
+    /**
+     * Plugin Version
+    */
+    if(!defined('afsdadsf_version')){
+      define('nextcare_api_service_plugin_version', '1.0.0');
+    } 
+    
+
+    /**
+     * Plugin URL
+    */
+    if(!defined('afsdadsf_version')){
+      define('nextcare_api_service_plugin_dir', plugin_dir_path(__FILE__));
+    } 
+  
+    require_once afsdadsf_dir . 'EnvReader.php';
+    
+ 
+?>

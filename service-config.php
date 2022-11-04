3a1314d398417da2c238663cf06f1163432dab07
ragulphc <ragul.p@avasoft.com>
2022-11-04 15:44:29 +0530

    Initial commit

---
+++ service-config.php
@@ -0,0 +1,12 @@
+<?php
+    $DEBUG = true;
+    
+    use asdfadsf_Env\EnvReader;
+    
+    (new EnvReader(asdfadsf_dir . '/env/config.env'))->load();
+    
+    $asfasdf = getenv(asfasdf); 
+$asfdasdf = getenv(asfdasdf); 
+
+
+?>
---
+++ servicelayer.php
@@ -0,0 +1,42 @@
+<?php 
+ 
+ /**
+    * asdfadsf 
+    *
+    * @package          asdfasfasd
+    * @author           afsdasfasdfasdf 
+    * @copyright        asdfasdfasdf   
+    * @license          asfasdfasdf 
+    * 
+    * @wordpress-plugin
+    * Plugin Name:      asdfadsf
+    * Plugin URI:       asdfasdfas 
+    * Description:      dfasdfas
+    * Version:          dfasdasf 
+    * Author:           asdasdfasdf
+    * Author URI:       afsfasdfffdsasf
+    * Text Domain:      adsfasdfasdfasdasd 
+    * License:          asfdasdfasdf 
+    * License URI:      asdfasdfasdffads 
+    */ 
+ 
+
+    /**
+     * Plugin Version
+    */
+    if(!defined('asdfadsf_version')){
+      define('nextcare_api_service_plugin_version', '1.0.0');
+    } 
+    
+
+    /**
+     * Plugin URL
+    */
+    if(!defined('asdfadsf_version')){
+      define('nextcare_api_service_plugin_dir', plugin_dir_path(__FILE__));
+    } 
+  
+    require_once asdfadsf_dir . 'EnvReader.php';
+    
+ 
+?>

dafad2c5668c04c4170cd828602f934768d4e1d2
ragulphc <ragul.p@avasoft.com>
2022-11-07 18:42:47 +0530

    Initial commit

---
+++ service-config.php
@@ -0,0 +1,11 @@
+<?php
+    $DEBUG = true;
+    
+    use dasfadsfsad_Env\EnvReader;
+    
+    (new EnvReader(dasfadsfsad_dir . '/env/config.env'))->load();
+    
+    $adsfasdf = getenv(adsfasdf); 
+
+
+?>
---
+++ servicelayer.php
@@ -0,0 +1,42 @@
+<?php 
+ 
+ /**
+    * dasfadsfsad 
+    *
+    * @package          asdfasdf
+    * @author           asdfasdf 
+    * @copyright        asdfasdf   
+    * @license          fasdfasdfsadf 
+    * 
+    * @wordpress-plugin
+    * Plugin Name:      dasfadsfsad
+    * Plugin URI:       fdasf 
+    * Description:      asdfasdf
+    * Version:          asdfasdsadf 
+    * Author:           adsfdassadf
+    * Author URI:       asdfasdfas
+    * Text Domain:      dfasdfasd 
+    * License:          asdfdsadf 
+    * License URI:      sadfasdfads 
+    */ 
+ 
+
+    /**
+     * Plugin Version
+    */
+    if(!defined('dasfadsfsad_version')){
+      define('nextcare_api_service_plugin_version', '1.0.0');
+    } 
+    
+
+    /**
+     * Plugin URL
+    */
+    if(!defined('dasfadsfsad_version')){
+      define('nextcare_api_service_plugin_dir', plugin_dir_path(__FILE__));
+    } 
+  
+    require_once dasfadsfsad_dir . 'EnvReader.php';
+    
+ 
+?>
